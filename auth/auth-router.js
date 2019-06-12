eab7858e64ce148d68c8b9bc47a822be654e322c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-12 10:35:12 -0500

    Guided Project - Auth(ii)
    Completed generating the token as mentioned in the TK video.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +20,38 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, //sub property - user id
+    username: user.username
+  };
+  const secret = "007";
+  const options = {
+    expiresIn: "8h"
+  };
+
+  return jwt.sign(payload, secrets.jwtsecret, options);
+}
 
 module.exports = router;
